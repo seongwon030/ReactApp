bb3fb94c72d164b3fd992faf78fcdcaa9ec4d7cd
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-01-30 16:40:24 +0900

    카카오톡 공유 기능 추가

--- music/src/FirstPage.js
+++ music/src/FirstPage.js
@@ -1,15 +1,36 @@
-import React from "react"
+import React from "react";
+import {useState, useRef} from "react";
 import { Link } from "react-router-dom";
 
 import './App.css';
 
 const FirstPage = () => {
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef(new Audio('./trap.mp3'));
+
+    const playMusic = () => {
+        const audio = audioRef.current;
+
+        if (audio.paused) {
+        audio.play(); 
+        } else {
+        audio.pause();
+        }
+        
+        setIsPlaying(!isPlaying);
+        // audio.currenTime = 0 // 클릭하면 다시 처음으로 
+    };
+
     return (
         <div className="firstPage">
             <Link to="/">
                 <img src="./back.png" alt="back"></img>
             </Link>
-            
+            <div className="num1">
+                <img onClick={playMusic} src="./play.png" alt='Sound Icon'/>
+                <h3>1번 사운드가 당신의 마음에 드나요?</h3>
+            </div>
         </div>
     );
 };
--- music/src/Main.js
+++ music/src/Main.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { useState,useRef } from "react";
+import { useEffect,useState,useRef } from "react";
 import { Link } from "react-router-dom";
 import './App.css';
+
+import { useScript } from './hooks';
 
 import {
     FacebookShareButton,
@@ -13,7 +15,21 @@ import {
 
 } from "react-share"
 
+import styled from "styled-components";
+
+const KakaoShareButton = styled.a`
+    cursor: pointer;
+`;
+const KakaoIcon = styled.img`
+	width: 48px;
+	height: 48px;
+	border-radius: 24px;
+`;
+
+
+
 const Main = () => {
+
     const [isPlaying, setIsPlaying] = useState(false);
     const audioRef = useRef(new Audio('./ex.mp3'));
 
@@ -31,6 +47,27 @@ const Main = () => {
     };
 
     const currentUrl = window.location.href; // 임시 url
+
+    // kakao SDK import하기
+	const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+	
+	// kakao sdk 초기화하기
+	// status가 변경될 때마다 실행되며, status가 ready일 때 초기화를 시도합니다.
+	useEffect(() => {
+		if (status === "ready" && window.Kakao) {
+			// 중복 initialization 방지
+			if (!window.Kakao.isInitialized()) {
+				// 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+				window.Kakao.init("00de026e315edfffd612d83e0fbce361");
+			}
+		}
+	}, [status]);	    
+
+    const handleKakaoButton = () => {
+        window.Kakao.Link.sendScrap({
+            requestUrl: currentUrl,
+        });
+    };
 
     return (
         <div className="New">
@@ -69,6 +106,9 @@ const Main = () => {
                 <LineShareButton style={{ marginRight: "20px" }} url={currentUrl}>
                     <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
                 </LineShareButton>
+                <KakaoShareButton onClick={handleKakaoButton}>
+					<KakaoIcon src="./kakao.png"></KakaoIcon>
+				</KakaoShareButton>
             </div>
         </div>
     );
