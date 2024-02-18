65c1389a13c9ab52dfe4ad54add10023df112e60
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-02-18 23:17:25 +0900

    체크박스 상태저장 완료

--- music/src/Components/CheckBox.js
+++ music/src/Components/CheckBox.js
@@ -1,50 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CheckBoxCnt from "./CheckBox.style";
-import '../App.css';
+import "../App.css";
+import { atom } from "recoil";
+import { useRecoilState } from "recoil";
+
+export const checkValueState = atom({
+  key: "checkValueState",
+  default: {},
+});
 
 const CheckBox = ({ id }) => {
-    // 체크 상태 관리 
-    const [checkedValue, setCheckedValue] = useState(null);
-    
-    // 중복 선택 안되게
-    const checkOnlyOne = (num) => {  
-        setCheckedValue((prevCheckedValue) => {
-            // 이미 선택된 값이면 현재값 반환
-            if (prevCheckedValue === num) {
-                return num;
-            }
-            // 선택된 값이 없거나 다른 값이면 현재 값으로 설정
-            return num;
-        });
-    };
-    
-    useEffect(() => { // 체크할 때 체크박스 id와 num 콘솔에 출력
-        console.log(id, checkedValue);  
-    }, [id,checkedValue]);
+  const [checkedValues, setCheckedValues] = useRecoilState(checkValueState);
 
-    const generateUniqueId = (name) => `${id}_${name}`;
+  const checkOnlyOne = (num) => {
+    setCheckedValues((prevCheckedValues) => {
+      const updatedValues = { ...prevCheckedValues, [id]: num };
+      return updatedValues;
+    });
+  };
 
+//   useEffect(() => {
+//     console.log(id, checkedValues[id]);
+//   }, [id, checkedValues]);
 
-    return (
-        
-        <CheckBoxCnt id={`checkBoxCnt_${id}`} num={checkedValue} >
-            {[1,2,3,4,5].map((num) => (
-                <React.Fragment key={num}>
-                    <input
-                        type="checkbox"
-                        id={generateUniqueId(`b${num}`)}
-                        name="checkWrap"
-                        value={num}
-                        checked={checkedValue === num}
-                        onChange={() => checkOnlyOne(num)}
-                    />
-                    <label 
-                    htmlFor={generateUniqueId(`b${num}`)}
-                    >{num}</label>
-                </React.Fragment>
-            ))}
-        </CheckBoxCnt>
-    );
-}
+  const generateUniqueId = (name) => `${id}_${name}`;
+
+  return (
+    <CheckBoxCnt id={`checkBoxCnt_${id}`} num={checkedValues[id]}>
+      {[1, 2, 3, 4, 5].map((num) => (
+        <React.Fragment key={num}>
+          <input
+            type="checkbox"
+            id={generateUniqueId(`b${num}`)}
+            name="checkWrap"
+            value={num}
+            checked={checkedValues[id] === num}
+            onChange={() => checkOnlyOne(num)}
+          />
+          <label htmlFor={generateUniqueId(`b${num}`)}>{num}</label>
+        </React.Fragment>
+      ))}
+    </CheckBoxCnt>
+  );
+};
 
 export default CheckBox;
