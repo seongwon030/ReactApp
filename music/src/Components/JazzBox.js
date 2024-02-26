04fb7c7a6eba5dabdb56b0f105d8a30e5b92c019
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-02-26 20:37:51 +0900

    Jazz State

---
+++ music/src/Components/JazzBox.js
@@ -0,0 +1,71 @@
+import React from "react";
+import CheckBoxCnt from "./CheckBox.style";
+import "../App.css";
+import { atom } from "recoil";
+import { useRecoilState } from "recoil";
+import { useEffect } from "react";
+
+
+export const jazzValueState = atom({
+  key: "Rock",
+  default: {},
+});
+
+const JazzBox = ({ id }) => {
+  const [jazzValues, setJazzValueState] = useRecoilState(jazzValueState);
+
+  const checkOnlyOne = (num) => {
+    setJazzValueState((prevCheckedValues) => {
+      if (id.includes("Jazz")) {
+      const updatedValues = { ...prevCheckedValues, [id]: num };
+      return updatedValues;
+    }
+    
+  });
+  };
+
+  useEffect(() => {
+    const handleCheckboxChange = () => {
+      console.log(id, jazzValues[id]);
+    };
+  
+    const checkboxElement = document.getElementById(`checkBoxCnt_${id}`);
+  
+    if (checkboxElement) {
+      checkboxElement.addEventListener('change', handleCheckboxChange);
+    }
+  
+    return () => {
+      if (checkboxElement) {
+        checkboxElement.removeEventListener('change', handleCheckboxChange);
+      }
+    };
+  }, [jazzValues, id]);
+
+  useEffect(() => { 
+    console.log("Jazz checkedValues:", jazzValues);
+  }, [jazzValues]);
+
+
+  const generateUniqueId = (name) => `${id}_${name}`;
+
+  return (
+    <CheckBoxCnt id={`checkBoxCnt_${id}`} num={jazzValues[id]}>
+      {[1, 2, 3, 4, 5].map((num) => (
+        <React.Fragment key={num}>
+          <input
+            type="checkbox"
+            id={generateUniqueId(`b${num}`)}
+            name="checkWrap"
+            value={num}
+            checked={jazzValues[id] === num}
+            onChange={() => checkOnlyOne(num)}
+          />
+          <label htmlFor={generateUniqueId(`b${num}`)}>{num}</label>
+        </React.Fragment>
+      ))}
+    </CheckBoxCnt>
+  );
+};
+
+export default JazzBox;
